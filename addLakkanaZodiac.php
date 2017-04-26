681222269d4092d50e4fae683b2e18af4a72fe5c
Iammmiind <Onanong.pureemahawong@gmail.com>
2017-04-26 18:56:30 +0700

    Finish backend

---
+++ addLakkanaZodiac.php
@@ -0,0 +1,11 @@
+<?php 
+    // allow all request can access
+    header('Access-Control-Allow-Origin: *');
+    
+    // connect database
+    $mysqli = new mysqli('localhost', 'root', '', 'HoroHolder');
+    $result = $mysqli->query("SELECT * FROM `zodiac` WHERE `id` = " . $_GET['id']);
+    $amount = $result->fetch_assoc()['amount'];
+    $mysqli->query("UPDATE `zodiac` SET `amount` = " . ($amount+1) . " WHERE `id` = " . $_GET['id']);    
+
+?>
---
+++ getTop3.php
@@ -0,0 +1,15 @@
+<?php 
+    // allow all request can access
+    header('Access-Control-Allow-Origin: *');
+    
+     // connect to the database
+    $mysqli = new mysqli('localhost', 'root', '', 'HoroHolder');
+    $result = $mysqli->query("SELECT * FROM `zodiac` ORDER BY amount DESC LIMIT 3");
+    
+    $arrayZodiacs = [];
+    while($data = $result->fetch_assoc()){
+        // push $data into $arrayZodiacs
+        array_push($arrayZodiacs, $data);
+    }
+    echo json_encode($arrayZodiacs);
+?>
